d0e6bf540ec3c6f9044f5c21c2bbe5fd5bbd6291
unknown <luk.matvey@yandex.ru>
2022-04-22 17:46:21 +0300

    Prepare to sent

--- Program.cs
+++ Program.cs
@@ -98,21 +98,22 @@
 // 8 7,8 -7,1 9
 
 
-void NewRandomArray(int m, int n)
-{
-    double[,] matrix = new double[m, n];
-    for (int i = 0; i < m; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            matrix[i, j] = new Random().NextDouble() * 200 - 100;
-            Console.Write("{0,0:F2} ", matrix[i, j]);
+// void NewRandomArray(int m, int n)
+// {
+//     double[,] matrix = new double[m, n];
+//     for (int i = 0; i < m; i++)
+//     {
+//         for (int j = 0; j < n; j++)
+//         {
+//             matrix[i, j] = new Random().NextDouble() * 200 - 100;
+//             Console.Write("{0,0:F2} ", matrix[i, j]);
 
-        }
-        Console.WriteLine();
-    }
-}
-NewRandomArray(2, 3);
+//         }
+//         Console.WriteLine();
+//     }
+// }
+// NewRandomArray(2, 3);
+
 
 
 //Задача 2. 
@@ -124,6 +125,39 @@ NewRandomArray(2, 3);
 // 8 4 2 4
 //1 7 -> такого числа в массиве нет
 
+// void Array (int m, int n)
+// {
+//     int [,] matrix = new int [m, n];
+//     for (int i = 0; i < m; i++)
+//     {
+//         for (int j = 0; j < n; j++)
+//         {
+//             matrix [i, j] = new Random().Next (-10, 10);
+//             Console.Write ($"{matrix [i,j]} ");
+//         }
+//         Console.WriteLine();
+//     }
+//     Console.Write ("Введите номер строки искомого элемента в матрице ");
+//     int row = Convert.ToInt32(Console.ReadLine());
+//     Console.Write ("Введите номер столбеца искомого элемента в матрице ");
+//     int col = Convert.ToInt32(Console.ReadLine());
+    
+//     if (row > 0 || col > 0 || row <= m || col <= n)
+//     {
+//         Console.Write ($"{matrix [row,col]} "); 
+//     }
+//     else
+//     {
+//         Console.Write ("Такого элемента не существует в матрице ");
+//     }
+        
+//     Console.WriteLine();
+    
+// }
+// int LengthRow = 3;
+// int LengthCol = 4;
+// Array (LengthRow, LengthCol);
+
 //Задача 3
 // Условие: Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
 
@@ -132,3 +166,36 @@ NewRandomArray(2, 3);
 // 5 9 2 3
 // 8 4 2 4
 // Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+void Array(int m, int n)
+{
+    int[,] matrix = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            matrix[i, j] = new Random().Next(-99, 100);
+            Console.Write($"{matrix[i,j]} ");
+
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+    for (int i = 0; i < n; i++)
+    {
+        double Avrg = 0;
+        for (int j = 0; j < m; j++)
+        {
+           Avrg = matrix[i,j] + Avrg; 
+        }
+        Console.Write("{0,0:F2} / ", Avrg/m);;
+    }
+    Console.WriteLine();
+        
+    
+}
+
+Array(3, 3);
+
+
+
